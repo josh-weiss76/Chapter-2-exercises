071a574aceb00deadd55654eec8f10724b5b7fbc
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-01 17:29:22 -0400

    Uploaded exercise files

---
+++ AreaOfARectangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaOfARectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length of the rectangle:");
+            string lengthInput = Console.ReadLine();
+            int length = int.Parse(lengthInput);
+            Console.WriteLine("Please enter the width of the rectangle:");
+            string widthInput = Console.ReadLine();
+            int width = int.Parse(widthInput);
+            Console.WriteLine("The area of the rectangle is " + (width * length));
+        }
+    }
+}
---
+++ StringSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchTheString = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine(searchTheString);
+            Console.WriteLine("Please select a word to search for from within the above sentence: ");
+            string userInput = Console.ReadLine();
+            string normalizedSentence = searchTheString.ToLower();
+            string normalizedInput = userInput.ToLower();
+            if (normalizedSentence.Contains(normalizedInput) == true) {
+                Console.WriteLine("The word '" + userInput + "' was found");
+            }
+            else
+            {
+                Console.WriteLine("The word '" + userInput + "' was not found");
+            }
+
+        }
+    }
+}
